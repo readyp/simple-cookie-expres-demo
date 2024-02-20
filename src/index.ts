6421be951ec5935a980c1286eef932e4b1a8ef51
readyp <readypamungkas@gmail.com>
2024-02-20 09:55:55 +0700

    init

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(helmet());
+app.use(cors({
+  origin: true,
+  Credentials: true,
+}));
+
+// cookie
+app.use(cookieParser());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  // check cookies
+  console.log('Cookies: ', req.cookies);
+  const cookies: null | string = req.cookies;
+  res.json({ message: 'Hello World', cookies });
+});
+
+app.get('/cookies', (req, res) => {
+  res.cookie('name', 'express', {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'none',
+  }).json({
+    message: 'Cookie set',
+  });
+});
+
+app.get('/clear-cookies', (req, res) => {
+  res.clearCookie('name').json({
+    message: 'Cookie cleared',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
